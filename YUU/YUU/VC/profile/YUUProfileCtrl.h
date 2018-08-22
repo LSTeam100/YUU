8ec56053b03cca9dcef318a54a7f6303e3328228
duanran <duanran@haima.me>
2018-08-23 00:03:27 +0800

    我的

---
+++ YUU/YUU/VC/profile/YUUProfileCtrl.h
@@ -0,0 +1,13 @@
+//
+//  YUUProfileCtrl.h
+//  YUU
+//
+//  Created by apple on 2018/8/22.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import "YUUSuperCtrl.h"
+
+@interface YUUProfileCtrl : YUUSuperCtrl
+
+@end
---
+++ YUU/YUU/VC/profile/YUUProfileCtrl.m
@@ -0,0 +1,43 @@
+//
+//  YUUProfileCtrl.m
+//  YUU
+//
+//  Created by apple on 2018/8/22.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import "YUUProfileCtrl.h"
+
+@interface YUUProfileCtrl ()
+@property(nonatomic,weak)IBOutlet UIButton *mineBtn;
+@property(nonatomic,weak)IBOutlet UIButton *billBtn;
+@property(nonatomic,weak)IBOutlet UIButton *coinBtn;
+@property(nonatomic,weak)IBOutlet UIButton *bussniessBtn;
+@property(nonatomic,weak)IBOutlet UIButton *costomBtn;
+@property(nonatomic,weak)IBOutlet UIButton *gamesBtn;
+
+@end
+
+@implementation YUUProfileCtrl
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
