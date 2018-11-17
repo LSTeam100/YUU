631fff75614994c67e1e16e2eab004c92542648c
duanran <duanran@haima.me>
2018-11-17 12:38:01 +0800

    游戏战报

--- YUU/YUU/VC/Games/GameHistroyCell.m
+++ YUU/YUU/VC/Games/GameHistroyCell.m
@@ -14,7 +14,15 @@
     [super awakeFromNib];
     self.timeLabel.textColor = colorWithHexString(@"52B2C6", 1.0);
     self.otherIdLabel.textColor = colorWithHexString(@"52B2C6", 1.0);
-    
+    self.firYuuLabel.textColor = colorWithHexString(@"ffa628", 1.0);
+    self.secYuuLabel.textColor = colorWithHexString(@"ffa628", 1.0);
+    self.thiYuuLabel.textColor = colorWithHexString(@"ffa628", 1.0);
+    self.firPohintLabel.textColor = colorWithHexString(@"ffa628", 1.0);
+    self.secPohintLabel.textColor = colorWithHexString(@"ffa628", 1.0);
+    self.thiPohintLabel.textColor = colorWithHexString(@"ffa628", 1.0);
+    self.bgView.layer.cornerRadius = 10;
+    self.bgView.layer.borderColor = [colorWithHexString(@"52B2C6", 1.0) CGColor];
+    self.bgView.layer.borderWidth = 1.0;
 
     // Initialization code
 }
